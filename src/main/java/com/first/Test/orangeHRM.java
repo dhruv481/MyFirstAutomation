0ddf82357c398e540f2051f3aa2eb8766a6fb74f
dhruv481 <dguptadg01@gmail.com>
2024-01-27 20:16:04 +0530

    adding xpaths

---
+++ src/main/java/com/first/Test/orangeHRM.java
@@ -0,0 +1,27 @@
+package com.first.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class orangeHRM {
+	WebDriver driver;
+	
+	@Test
+	public void launchApp () {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");		
+	}
+	
+	@Test
+	 public void enterLoginDetails() throws Exception {
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+		driver.findElement(clickLoginButton);
+	}
+}
---
+++ src/main/java/com/first/xpaths/allXpaths.java
@@ -0,0 +1,13 @@
+package com.first.xpaths;
+
+import javax.xml.xpath.XPath;
+
+public class allXpaths {
+
+	public void paths() {
+		
+	}
+	String clickLoginButton = XPath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button");
+
+	
+}
